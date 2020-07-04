ab1fdd73c332bdf5c29e196f47ec473824ccf1be
Haroldo Rodriguez Salgado <haroldo.rodriguez.salgado@gmail.com>
2020-07-04 03:22:47 -0400

    Actualizacion

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Brainiac</title>
+    <meta http-equiv="Content-Language" content="es-cl">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link type="text/css" href="bootstrap337dist/css/bootstrap.css" rel="stylesheet">
+    <link type="text/css" href="fontawesome470/css/font-awesome.css" rel="stylesheet">
+
+    <link type="text/css" href="DataTables/DataTables11016/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <link type="text/css" href="css/Style001.min.css" rel="stylesheet">
+    <link type="text/css" href="css/CssAutoGenerado.css" rel="stylesheet">
+
+    <script type="text/javascript" src="jquery321min/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="bootstrap337dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="DataTables/DataTables11016/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="DataTables/DataTables11016/js/dataTables.bootstrap4.min.js"></script>
+
+    <script type="text/javascript" src="js/constructor.min.js"></script>
+    <script type="text/javascript" src="js/interfaz.min.js"></script>
+    <script type="text/javascript" src="js/validador.min.js"></script>
+    <script type="text/javascript" src="js/especiales.min.js"></script>
+
+</head>
+<body onload="javascript:NoBack()" onsubmit="return false">
+<script>
+funconstructor001();
+</script>
+</body>
+</html>
