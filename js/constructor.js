f3803fd88c2915443f6aae096579a75e724cef67
Haroldo Rodriguez Salgado <haroldo.rodriguez.salgado@gmail.com>
2020-07-11 03:03:48 -0400

    Actualizacion

--- js/constructor.js
+++ js/constructor.js
@@ -94,10 +94,13 @@ function funconstructor002() {
             $('#idbuttonfuncons002').append('<span class="navbar-toggler-icon"></span>');
         $('#idnavbarfuncons002').append('<div id="iddivcollapsefuncons002" class="collapse navbar-collapse" id="navbarTogglerDemo01"></div>');
             $('#iddivcollapsefuncons002').append('<a class="navbar-brand" href="#">Brainiac</a>    ');
-            $('#iddivcollapsefuncons002').append('<ul id="idulfuncons002" class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>');
-                $('#idulfuncons002').append('<li class="nav-item"><a class="nav-link" href="#">Panel<span class="sr-only">(current)</span></a></li>');
-                $('#idulfuncons002').append('<li class="nav-item"><a class="nav-link" href="#">Monitoreo</a></li>');
-                $('#idulfuncons002').append('<li class="nav-item"><a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a></li>');
+            $('#iddivcollapsefuncons002').append('<ul id="idul001funcons002" class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>');
+                $('#idul001funcons002').append('<li class="nav-item"><a class="nav-link" href="#">Panel<span class="sr-only">(current)</span></a></li>');
+                $('#idul001funcons002').append('<li class="nav-item"><a class="nav-link" href="#">Monitoreo</a></li>');
+                /* $('#idulfuncons002').append('<li class="nav-item"><a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a></li>'); */
+            
+            $('#iddivcollapsefuncons002').append('<ul id="idul002funcons002" class="navbar-nav my-2 my-lg-0"></ul>');
+                $('#idul002funcons002').append('<li class="nav-item"><a class="nav-link" href="#">Cerrar</a></li>');
 
 
     /*Fin de la creacion de la Barra Superior*/
