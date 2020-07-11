7f5c0a5218cb4d34d8d0913fcb80db95cc684602
Haroldo Rodriguez Salgado <haroldo.rodriguez.salgado@gmail.com>
2020-07-11 00:18:13 -0400

    Actualizacion

--- js/constructor.js
+++ js/constructor.js
@@ -18,7 +18,7 @@ function funconstructor001() {
                         $('#idinputgroup002funcon001').append('<span class="input-group-addon"><i class="glyphicon glyphicon-lock" style="width:auto"></i></span><input type="password" id="IDInputPassword" runat="server" class="form-control input-lg" name="user" placeholder="Password" onkeydown="return ValidarEnter(event)" required></input>');
                 $('#iddivpanelbodyfuncon001').append('<button id="BotonValidar" runat="server" class="btn btn-info btn-lg" style="width: 100%" onclick="fun_login()">Validar</button>'); */
         $('body').append('<div id="iddivcontainerfuncon001" class="container col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4" style="padding-top:100px"></div>');
-            $('#iddivcontainerfuncon001').append('<div id="iddivcardfuncons001" class="card"></div>');
+            $('#iddivcontainerfuncon001').append('<div id="iddivcardfuncons001" class="card text-white bg-dark mb-3" style="max-width: 18rem;"></div>');
                 $('#iddivcardfuncons001').append('<img src="images/SmallLogo.png" width="250" height"301" class="card-img-top" alt="Brainiac"></img>');
                 $('#iddivcardfuncons001').append('<div id="iddivcardheaderfuncon001" class="card-header">Formulario Acceso</div>');
 
