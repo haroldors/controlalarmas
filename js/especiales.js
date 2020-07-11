17395bb94eeb48b88f46958365eb2df470f97912
Haroldo Rodriguez Salgado <haroldo.rodriguez.salgado@gmail.com>
2020-07-11 16:04:53 -0400

    Actualizacion

--- js/especiales.js
+++ js/especiales.js
@@ -4,3 +4,13 @@ function NoBack() {
     window.location.hash="Again-No-back-button" //chrome
     window.onhashchange=function(){window.location.hash="no-back-button";}
     };
+
+function funmensajedeenvio() {
+    if (document.getElementById('IDDivProcesando001').style.display == "none") {
+        document.getElementById('IDDivProcesando001').style.display = "block";
+        document.getElementById('IDDivProcesando002').style.display = "block";
+    } else {
+        document.getElementById('IDDivProcesando001').style.display = "none";
+        document.getElementById('IDDivProcesando002').style.display = "none";
+    }
+}    
--- js/validador.js
+++ js/validador.js
@@ -10,4 +10,6 @@ function link002() {
 	funconstructor002(); /* Construye la estructura marco */
 	funconstructor003(); /* construye la estructura de la tabla */
 	funconstructor004(); /* construye la estructura interna de la tabla de sesnsores */
+	funmensajedeenvio();
 }
+
