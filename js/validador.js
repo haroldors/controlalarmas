a430aab89306473030ee8e07ccf5fc9839acc8c5
Haroldo Rodriguez Salgado <haroldo.rodriguez.salgado@gmail.com>
2020-07-12 08:38:49 -0400

    Actualizacion

--- js/validador.js
+++ js/validador.js
@@ -3,6 +3,7 @@ function link001() {
 
 	document.getElementById('idbuttonaceptarfuncons001').onclick = function() {
 		link002();
+		link003();
 	}
 }
 
@@ -45,5 +46,28 @@ function link002() {
 					''
 					), 2000);			
 		}
+}
+
+/*la siguiente funcion modifica la distribucion de los div dentro de la ventana*/
+function link003() {
+    if (document.getElementById('idcolumnaderechafuncons002').className == 'col-xs-12 col-sm-3') {
+		/* document.getElementById('idcolumnaderechafuncons002').className = 'col-xs-12'; */
+		document.getElementById('idcolumnaderechafuncons002').style.display='block';
+		/* document.getElementById('idcolumnaizquierdafuncons002').className = 'col-xs-12 col-sm-3'; */
+/*         document.getElementById('idlinkredimencion').className = 'fa fa-plus-square-o fa-1x';
+        document.getElementById('idcolumnaderecha').className = 'col-xs-12';
+        document.getElementById('idcolumnaizquierda').className = 'col-xs-12';
+        document.getElementById('idpanelbodyizquierdo').style.display = 'none';
+        document.getElementById('idbuttonlateral').style.display='block';
+        document.getElementById('idcolumnaderecha').style.display='none'; */
+    } else {
+		document.getElementById('idcolumnaderechafuncons002').style.display='none';
+/*         document.getElementById('idlinkredimencion').className = 'fa fa-minus-square-o fa-1x';
+        document.getElementById('idcolumnaderecha').className = 'col-xs-12 col-sm-3';
+        document.getElementById('idcolumnaizquierda').className = 'col-xs-12 col-sm-9';
+        document.getElementById('idpanelbodyizquierdo').style.display = 'block';
+        document.getElementById('idbuttonlateral').style.display='none';
+        document.getElementById('idcolumnaderecha').style.display='block'; */
+    }
 }
 
